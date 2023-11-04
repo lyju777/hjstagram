2263e462a86767af29210c929a56dabc921a9543
lyju777 <lyju777@hanmail.net>
2023-11-04 17:02:46 +0900

    상세프로필 게시글 삭제 기능 수정

--- frontend/src/components/profile/profile_post.jsx
+++ frontend/src/components/profile/profile_post.jsx
@@ -1,110 +1,105 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Modal_Delete_Posts from "./modal_delete_posts";
 import axios from "axios";
 import { withRouter, useHistory } from "react-router-dom";
 
+function Profile_Post(props) {
+  const [Posts, setPosts] = useState([]);
 
+  const PostsArr = [];
+  const [ID, setID] = useState("");
 
+  const saveIDandModal = (id) => {
+    setID(id);
+    modal_change(true);
+    console.log(ID);
+  };
 
-function Profile_Post(props){
+  useEffect(() => {
+    axios.get("/api/auth/check").then((response) => {
+      console.log("로그인한 사람 username : " + response.data.username);
+      const logusername = response.data.username;
+      axios.get(`/api/posts?username=${logusername}`).then((response) => {
+        console.log(response);
 
-    const [Posts, setPosts] = useState([]);
+        for (let i = 0; i < response.data.length; i++) {
+          console.log(response.data[i].fileurls[0]);
 
-    const PostsArr = [];
-    const [ID, setID] = useState("");
+          PostsArr[i] = response.data[i];
+          setPosts([...PostsArr]);
+        }
+      });
+    });
+  }, []);
 
-    const saveIDandModal = (id) => {
-      setID(id);
-      modal_change(true);
-      console.log(ID);
-    };
+  const deletePost = (ID) => {
+    axios.delete(`/api/posts/${ID}`).then((response) => {
+      console.log("삭제됨!");
 
-    useEffect(() => {
-        axios.get('/api/auth/check')
-        .then(response => {
-            console.log("로그인한 사람 username : "+response.data.username);
-            const logusername = response.data.username;
-            axios.get(`/api/posts?username=${logusername}`)
-            .then(response => {
-                console.log(response);
-              
-                for(let i=0; i<response.data.length; i++){
-                    console.log(response.data[i].fileurls[0]);
+      axios.patch("/api/auth/removePost").then((res) => {
+        console.log("게시물 -1");
 
-                    PostsArr[i] = response.data[i];
-                    setPosts([...PostsArr]);
-                }
-            })
-        })
-    },[]);
+        // 게시물 삭제 후에 게시물 목록을 다시 불러옵니다.
+        axios.get("/api/auth/check").then((response) => {
+          const logusername = response.data.username;
+          axios.get(`/api/posts?username=${logusername}`).then((response) => {
+            const updatedPosts = response.data;
+            setPosts(updatedPosts);
+          });
+        });
+      });
+    });
+  };
 
+  let [modal, modal_change] = useState(false);
 
+  const closeModal = () => {
+    modal_change(false);
+  };
 
-    const deletePost = (ID) => {
-        axios.delete(`/api/posts/${ID}`)
-        .then(response => {
-            console.log("삭제됨!");
+  return (
+    <>
+      <hr className="profile_hr"></hr>
 
-            axios.patch('/api/auth/removePost')
-            .then(res => {
-                console.log("게시물 -1");
-                window.location.reload();
-            })
-        })
-    }
-    
+      <div className="profile_div_box">
+        {Posts &&
+          Posts.map((a) => {
+            return (
+              <div className="profile_div_imgbox">
+                <img
+                  alt=""
+                  className="profile_div_img"
+                  src={a.fileurls[0]}
+                  onClick={() => {
+                    saveIDandModal(a._id);
+                  }}
+                />
+              </div>
+            );
+          })}
+      </div>
 
+      {modal === true ? (
+        <div className="modal_background" onClick={closeModal}>
+          <div className="modal_main_point modal_editprofile_point">
+            <div className="modal_main_point_text modal_editprofile_text">
+              <div>
+                <p>게시물을 삭제 하시겠습니까?</p>
+              </div>
 
-    let [modal , modal_change] = useState(false);
-
-    const closeModal = () => {
-        modal_change(false);
-      }
-
-    return(
-        <>
-        <hr className="profile_hr"></hr>
-
-    
-        <div className="profile_div_box">
-
-
-{
-    Posts && Posts.map((a) => {
-        return(
-            <div className="profile_div_imgbox">
-            <img className="profile_div_img" src={a.fileurls[0]} onClick={()=>{saveIDandModal(a._id)}}/>
+              <button
+                type="submit"
+                onClick={() => deletePost(ID)}
+                className="btn btn-primary modal_editprofile_btn"
+              >
+                삭제
+              </button>
             </div>
-        )
-    })
-}
-
-
-
-
+          </div>
         </div>
-
-
-    {
-    modal === true
-    ? <div className="modal_background" onClick={closeModal}>
-      <div className="modal_main_point modal_editprofile_point">
-      <div className="modal_main_point_text modal_editprofile_text">
-
-      <div>
-        <p>게시물을 삭제 하시겠습니까?</p>
-      </div>
-
-      <button type="submit" onClick={() => deletePost(ID)} className="btn btn-primary modal_editprofile_btn">삭제</button>
-      </div>
-      </div>
-      </div>
-    : null
-    }
-
-
-      </>
-    )
+      ) : null}
+    </>
+  );
 }
 
 export default withRouter(Profile_Post);
