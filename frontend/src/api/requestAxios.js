6fe23ec2cb9d6379913c7e79c770a34a0898719b
lyju777 <lyju777@hanmail.net>
2024-01-07 12:24:26 +0900

    Access-Control-Allow-Origin 설정

--- frontend/src/api/requestAxios.js
+++ frontend/src/api/requestAxios.js
@@ -4,6 +4,8 @@ const requestAxios = axios.create({
 	baseURL : process.env.REACT_APP_API,
 	headers: {
     'Content-type': 'application/json',
+		'Access-Control-Allow-Origin' : '*',
+		'Access-Control-Allow-Methods' : '*'
   },
 	withCredentials: true
 })
