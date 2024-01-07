398a2c5fd4fc4f2b934862e81bbb78f51722c4d5
lyju777 <lyju777@hanmail.net>
2024-01-07 16:52:34 +0900

    requestAxios headers제거

--- frontend/src/api/requestAxios.js
+++ frontend/src/api/requestAxios.js
@@ -2,11 +2,11 @@ import axios from 'axios'
 
 const requestAxios = axios.create({
 	baseURL : process.env.REACT_APP_API,
-	headers: {
-    'Content-type': 'application/json',
-		'Access-Control-Allow-Origin' : '*',
-		'Access-Control-Allow-Methods' : '*'
-  },
+	// headers: {
+  //   'Content-type': 'application/json',
+	// 	'Access-Control-Allow-Origin' : '*',
+	// 	'Access-Control-Allow-Methods' : '*'
+  // },
 	withCredentials: true
 })
 
