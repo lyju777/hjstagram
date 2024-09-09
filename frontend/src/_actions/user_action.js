3daf36b4a6fa7c534c916ea0605814860e7bd20a
lyju777 <lyju777@hanmail.net>
2024-09-09 19:39:06 +0900

    login로직 console.err 추가

--- frontend/src/_actions/user_action.js
+++ frontend/src/_actions/user_action.js
@@ -6,17 +6,18 @@ import {
     SEND_EMAIL
 } from './types';
 
-export function LoginUser(dataToSubmit) {   // body를 dataToSubmit파라미터를 통해 전달받음
-
-const request = requestAxios.post('api/auth/login', dataToSubmit)  // 서버에서 받은 data를 request에 저장
-    .then(response => response.data)
+export function LoginUser(dataToSubmit) {
+    // body를 dataToSubmit파라미터를 통해 전달받음
+    const request = requestAxios.post('api/auth/login', dataToSubmit)  // 서버에서 받은 data를 request에 저장
+        .then(response => response.data)
+        .catch(err => console.log(err)); // 에러 발생 시 콘솔에 출력
 
     // return을 시켜서 reducer 로 보냄 
     return{ 
         type: LOGIN_USER,
         payload: request
     }
-} 
+}
 
 export function registerUser(dataToSubmit) {
 
