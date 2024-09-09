770b695347b4fc337b25424cce38f4599594236f
lyju777 <lyju777@hanmail.net>
2024-09-09 20:36:36 +0900

    cors credentials설정수정

--- frontend/src/api/requestAxios.js
+++ frontend/src/api/requestAxios.js
@@ -2,12 +2,7 @@ import axios from 'axios'
 
 const requestAxios = axios.create({
 	baseURL : process.env.REACT_APP_API,
-	// headers: {
-  //   'Content-type': 'application/json',
-	// 	'Access-Control-Allow-Origin' : '*',
-	// 	'Access-Control-Allow-Methods' : '*'
-  // },
-	withCredentials: true
-})
+	withCredentials: true,
+});
 
 export default requestAxios
